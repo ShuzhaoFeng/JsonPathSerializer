2367981a7157b93f9a58466246171b4123988af4
Shuzhao Feng <shuzhao_feng@outlook.com>
2022-09-21 09:45:21 -0400

    First commit

---
+++ src/PathGlobals.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace JsonPathSerializer
+{
+    internal class PathGlobals
+    {
+        public class PathRegex
+        {
+            public static readonly Regex KEY = new(@"(\[\'[^']+\'\])");
+            public static readonly Regex INDEX = new(@"(\[[0-9]+\])");
+
+            public static readonly Regex BRACKET = new(@"(\[[^\]]+\])");
+            public static readonly Regex PARENTHESIS_WITHIN_BRACKET = new(@"(\[\([^\)]+\)\])");
+        }
+    }
+}
