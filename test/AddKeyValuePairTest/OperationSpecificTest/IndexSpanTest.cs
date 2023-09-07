04eaf95efcef1ce4e1e1e2ef8c87e7fbee039dbf
Shuzhao Feng <90885757+ShuzhaoFeng@users.noreply.github.com>
2023-09-07 10:16:09 -0400

    Expand Index Span Test (#38)

--- test/AddKeyValuePairTest/OperationSpecificTest/IndexSpanTest.cs
+++ test/AddKeyValuePairTest/OperationSpecificTest/IndexSpanTest.cs
@@ -34,9 +34,13 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
         {
             _emptyManager.Add("name[0:2]", "Shuzhao");
 
+            // indexes that should be affected
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][0].ToString());
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][1].ToString());
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][2].ToString());
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][3].ToString());
         }
 
         [TestMethod]
@@ -44,9 +48,13 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
         {
             _emptyManager.Add("[0:2]", "Shuzhao Feng");
 
+            // indexes that should be affected
             Assert.AreEqual("Shuzhao Feng", _emptyManager.Value[0].ToString());
             Assert.AreEqual("Shuzhao Feng", _emptyManager.Value[1].ToString());
             Assert.AreEqual("Shuzhao Feng", _emptyManager.Value[2].ToString());
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value[3].ToString());
         }
 
         [TestMethod]
@@ -54,9 +62,14 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
         {
             _emptyManager.Add("name[0][0:2]", "Shuzhao");
 
+            // indexes that should be affected
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][0][0].ToString());
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][0][1].ToString());
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][0][2].ToString());
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][1].ToString());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][0][3].ToString());
         }
 
         [TestMethod]
@@ -64,9 +77,16 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
         {
             _emptyManager.Add("name[0:2][0]", "Shuzhao");
 
+            // indexes that should be affected
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][0][0].ToString());
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][1][0].ToString());
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][2][0].ToString());
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][3].ToString());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][0][1].ToString());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][0][2].ToString());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][0][3].ToString());
         }
 
         [TestMethod]
@@ -74,6 +94,7 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
         {
             _emptyManager.Add("name[0:2][0:2]", "Shuzhao");
 
+            // indexes that should be affected
             // reduce 9 assertions to a for loop
             for (int i = 0; i < 3; i++)
             {
@@ -81,6 +102,12 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
                 Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][i][1].ToString());
                 Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][i][2].ToString());
             }
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][3].ToString());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][0][3].ToString());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][1][3].ToString());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][2][3].ToString());
         }
 
         [TestMethod]
@@ -88,10 +115,16 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
         {
             _loadedManager.Add("name[0:2]", "John Doe");
 
+            // indexes that should be affected
             Assert.AreEqual("John Doe", _loadedManager.Value["name"][0].ToString());
             Assert.AreEqual("John Doe", _loadedManager.Value["name"][1].ToString());
             Assert.AreEqual("John Doe", _loadedManager.Value["name"][2].ToString());
 
+            // indexes that should not be affected
+            Assert.AreEqual("SF", _loadedManager.Value["name"][3].ToString());
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _loadedManager.Value["name"][4].ToString());
         }
 
         [TestMethod]
@@ -99,11 +132,18 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
         {
             _loadedManager.Add("name[1:5]", "John Doe");
 
+            // indexes that should be affected
             // reduce 9 assertions to a for loop
             for (int i = 1; i < 6; i++)
             {
                 Assert.AreEqual("John Doe", _loadedManager.Value["name"][i].ToString());
             }
+
+            // indexes that should not be affected
+            Assert.AreEqual("Shuzhao", _loadedManager.Value["name"][0].ToString());
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _loadedManager.Value["name"][6].ToString());
         }
 
         [TestMethod]
@@ -111,10 +151,20 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
         {
             _emptyManager.Add("name[-5:-3]", "Shuzhao");
 
+            // we need minimally 5 elements: 0 (-5), 1 (-4), 2 (-3), 3 (-2) and 4 (-1)
+
+            // indexes that should be affected
             // C# array doesn't allow negative index value (but we do), so -5 is converted to 0 and -3 to 2.
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][0].ToString());
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][1].ToString());
             Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][2].ToString());
+
+            // empty indexes added to fill the gap
+            Assert.AreEqual("{}", _emptyManager.Value["name"][3].ToString());
+            Assert.AreEqual("{}", _emptyManager.Value["name"][4].ToString());
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][5].ToString());
         }
 
         [TestMethod]
@@ -127,6 +177,69 @@ namespace JsonPathSerializerTest.AddKeyValuePairTest.OperationSpecificTest
             {
                 Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][i].ToString());
             }
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][8].ToString());
+        }
+
+        [TestMethod]
+        public void CanAddReverseIndexSpan()
+        {
+            _emptyManager.Add("name[5:2]", "Shuzhao");
+
+            // empty indexes added to fill the gap
+            Assert.AreEqual("{}", _emptyManager.Value["name"][0].ToString());
+            Assert.AreEqual("{}", _emptyManager.Value["name"][1].ToString());
+
+            // indexes that should be affected
+            // reduce 4 assertions to a for loop
+            for (int i = 2; i < 6; i++)
+            {
+                Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][i].ToString());
+            }
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][6].ToString());
+        }
+
+        [TestMethod]
+        public void CanAddReverseNegativeIndexSpan()
+        {
+            _emptyManager.Add("name[-2:-5]", "Shuzhao");
+
+            // we need minimally 5 elements: 0 (-5), 1 (-4), 2 (-3), 3 (-2) and 4 (-1)
+
+            // indexes that should be affected
+            // C# array doesn't allow negative index value (but we do), so -5 is converted to 0 and etc.
+            // reduce 4 assertions to a for loop
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][i].ToString());
+            }
+
+            // empty indexes added to fill the gap
+            Assert.AreEqual("{}", _emptyManager.Value["name"][4].ToString());
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][5].ToString());
+        }
+
+        [TestMethod]
+        public void CanAddReversePositiveAndNegativeIndexSpan()
+        {
+            _emptyManager.Add("name[2:-5]", "Shuzhao");
+
+            // we need minimally 8 elements: 0, 1, 2, 3 (-5), 4 (-4), 5 (-3), 6 (-2) and 7 (-1)
+
+            // indexes that should be affected
+            // reduce 8 assertions to a for loop
+            for (int i = 0; i < 8; i++)
+            {
+                Assert.AreEqual("Shuzhao", _emptyManager.Value["name"][i].ToString());
+            }
+
+            // no extra indexes are added
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _emptyManager.Value["name"][8].ToString());
         }
 
         [TestMethod]
