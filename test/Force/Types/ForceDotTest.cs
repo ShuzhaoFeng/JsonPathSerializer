d15ff56c389638a4c7d6da55eb163838583770ab
ShuzhaoFeng <shuzhao_feng@outlook.com>
2023-09-11 13:59:13 -0400

    Add Force Dot Test

---
+++ test/Force/Types/ForceDotTest.cs
@@ -0,0 +1,93 @@
+﻿namespace JsonPathSerializerTest.Force.Types
+{
+    [TestClass]
+    public class ForceDotTest
+    {
+        private JsonPathManager _emptyManager = new();
+        private JsonPathManager _loadedManager = new();
+        private JsonPathManager _indexedManager = new();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _emptyManager = new JsonPathManager();
+            _loadedManager = new JsonPathManager(@"{
+                ""name"": {
+                    ""first"" : ""Shuzhao"",
+                },
+            }");
+            _indexedManager = new JsonPathManager(@"{
+                ""name"": [
+                    ""Shuzhao"",
+                ],
+            }");
+        }
+
+        [TestMethod]
+        public void CanForceKey()
+        {
+            _emptyManager.Force("name", "Shuzhao Feng");
+
+            Assert.AreEqual("Shuzhao Feng", _emptyManager.Value["name"].ToString());
+        }
+
+        [TestMethod]
+        public void CanForceNestedKey()
+        {
+            _emptyManager.Force("name.first", "Shuzhao");
+
+            Assert.AreEqual("Shuzhao", _emptyManager.Value["name"]["first"].ToString());
+        }
+
+        [TestMethod]
+        public void CanForceInsertKeyUnderExistingParentKey()
+        {
+            _loadedManager.Force("name.last", "Feng");
+
+            Assert.AreEqual("Shuzhao", _loadedManager.Value["name"]["first"].ToString());
+            Assert.AreEqual("Feng", _loadedManager.Value["name"]["last"].ToString());
+        }
+
+        [TestMethod]
+        public void CanForceInsertValueToParentKey()
+        {
+            _loadedManager.Force("name", "Shuzhao Feng");
+
+            Assert.AreEqual("Shuzhao Feng", _loadedManager.Value["name"].ToString());
+
+            Assert.ThrowsException<InvalidOperationException>(() => _loadedManager.Value["name"]["first"].ToString());
+        }
+
+        [TestMethod]
+        public void CanForceInsertValueToArray()
+        {
+            _indexedManager.Force("name.last", "Feng");
+
+            Assert.AreEqual("Feng", _indexedManager.Value["name"]["last"].ToString());
+
+            Assert.ThrowsException<ArgumentException>(() => _indexedManager.Value["name"][0].ToString());
+        }
+
+        [TestMethod]
+        public void CanForceInsertValueAsChildUnderExistingValue()
+        {
+            _loadedManager.Force("name.first.English", "Shuzhao");
+
+            Assert.AreEqual("Shuzhao", _loadedManager.Value["name"]["first"]["English"].ToString());
+        }
+
+        [TestMethod]
+        public void ThrowsExceptionWhenForcingKeyWithDoubleDots()
+        {
+            Assert.ThrowsException<ArgumentException>(() => _emptyManager.Force("name..last", "Feng"));
+        }
+
+        [TestMethod]
+        public void ThrowsExceptionWhenForcingKeyWithEndingDot()
+        {
+            Assert.ThrowsException<JsonException>(() => _emptyManager.Force("name.last.", "Feng"));
+        }
+    }
+}
+
+
