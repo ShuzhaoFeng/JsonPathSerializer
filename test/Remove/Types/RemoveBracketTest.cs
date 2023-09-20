9fea89af68f3280e52e850f9468adc8dd04bb808
Shuzhao Feng <90885757+ShuzhaoFeng@users.noreply.github.com>
2023-09-20 13:58:56 -0400

    Add Remove Bracket Test (#58)

--- test/Remove/Types/RemoveBracketTest.cs
+++ test/Remove/Types/RemoveBracketTest.cs
@@ -3,9 +3,73 @@
     [TestClass]
     public class RemoveBracketTest
     {
+        private JsonPathManager _loadedManager = new();
+        private JsonPathManager _loadedBigManager = new();
+
+
         [TestInitialize]
         public void Initialize()
         {
+            _loadedManager = new JsonPathManager(@"{
+                ""name"": ""Shuzhao Feng"",
+            }");
+
+            _loadedBigManager = new JsonPathManager(@"{
+                ""name"": {
+                    ""first"": {
+                        ""whole"": ""Shuzhao"",
+                        ""split"": ""Shu Zhao"",
+                    },
+                    ""last"": ""Feng"",
+                },
+            }");
+        }
+
+        [TestMethod]
+        public void CanRemoveProperty()
+        {
+            var removed = _loadedManager.Remove("['name']");
+
+            Assert.AreEqual("Shuzhao Feng", removed?.ToString());
+            Assert.AreEqual(null, _loadedManager.Value["name"]);
+        }
+
+        [TestMethod]
+        public void CanRemovePropertyWithChildren()
+        {
+            var removed = _loadedBigManager.Remove("['name']['first']");
+
+            // children are removed
+            Assert.AreEqual("Shuzhao", removed?["whole"]);
+            Assert.AreEqual("Shu Zhao", removed?["split"]);
+
+            // key is removed from root
+            Assert.AreEqual(null, _loadedBigManager.Value["name"]["first"]);
+
+            // other keys are untouched
+            Assert.AreEqual("Feng", _loadedBigManager.Value["name"]["last"].ToString());
+
+            // removed children are not accessible
+            Assert.ThrowsException<InvalidOperationException>(() => _loadedManager.Value["name"]["first"]["whole"]);
+            Assert.ThrowsException<InvalidOperationException>(() => _loadedManager.Value["name"]["first"]["split"]);
+        }
+
+        [TestMethod]
+        public void ThrowsExceptionWhenRemovingPropertyWithStringInBracketWithoutQuote()
+        {
+            Assert.ThrowsException<JsonException>(() => _loadedManager.Remove("name[last]"));
+        }
+
+        [TestMethod]
+        public void ThrowsExceptionWhenRemovingPropertyWithUnclosedBracket()
+        {
+            Assert.ThrowsException<JsonException>(() => _loadedManager.Remove("name['last'"));
+        }
+
+        [TestMethod]
+        public void ThrowsExceptionWhenRemovingPropertyWithUnopenedBracket()
+        {
+            Assert.ThrowsException<JsonException>(() => _loadedManager.Remove("name'last']"));
         }
     }
 }
