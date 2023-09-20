d16677b860d2c4466f0c7748f49d46bce4672896
Shuzhao Feng <90885757+ShuzhaoFeng@users.noreply.github.com>
2023-09-19 20:54:00 -0400

    Implement Remove Function (#55)
    * Implement Remove Function
    
    * Optimize switch statement

--- src/Utils/JTokenRemover.cs
+++ src/Utils/JTokenRemover.cs
@@ -8,7 +8,87 @@ namespace JsonPathSerializer.Utils
     {
         public static JToken? Remove(JArray parent, JsonPathToken token)
         {
-            throw new NotImplementedException();
+            JToken? removed;
+
+            switch (token.Type)
+            {
+                case JsonPathToken.TokenType.Index:
+                    removed = parent[(int) token.Value];
+                    parent.RemoveAt((int) token.Value);
+                    break;
+
+                case JsonPathToken.TokenType.Indexes:
+                    JArray arrayToKeep = new JArray();
+                    JArray arrayToRemove = new JArray();
+                    List<int> indexes = (List<int>) token.Value;
+
+                    for (int i = 0; i < indexes.Count; i++)
+                    {
+                        // If the index is negative, it is relative to the end of the array.
+                        if (indexes.Contains(i) || indexes.Contains(i - indexes.Count))
+                        {
+                            arrayToRemove.Add(parent[i]);
+                        }
+                        else
+                        {
+                            arrayToKeep.Add(parent[i]);
+                        }
+                    }
+
+                    parent.Replace(arrayToKeep);
+
+                    removed = arrayToRemove;
+
+                    break;
+
+                case JsonPathToken.TokenType.IndexSpan:
+                    IndexSpanValueContainer indexSpan = (IndexSpanValueContainer) token.Value;
+
+                    arrayToKeep = new JArray();
+                    arrayToRemove = new JArray();
+
+                    int start = indexSpan.StartIndex;
+                    int end = indexSpan.EndIndex ?? parent.Count - 1;
+
+                    for (int i = 0; i < parent.Count; i++)
+                    {
+                        bool isInRange = (start >= 0, end >= 0) switch
+                        {
+                            (true, true) when start < end => // spans from start to end
+                                i >= start && i <= end,
+                            (true, true) => // spans from end to start
+                                i >= end && i <= start,
+                            (false, true) => // spans from 0 to end, then from start to Count - 1
+                                i >= 0 && i <= end || i >= parent.Count + start && i < parent.Count,
+                            (true, false) => // spans from 0 to start, then from end to Count - 1
+                                i >= 0 && i <= start || i >= parent.Count + end && i < parent.Count,
+                            (false, false) when start < end => // spans from start to end
+                                i >= parent.Count + start && i <= parent.Count + end,
+                            (false, false) => // spans from end to start
+                                i >= parent.Count + end && i <= parent.Count + start
+                        };
+
+                        if (isInRange)
+                        {
+                            arrayToRemove.Add(parent[i]);
+                        }
+                        else
+                        {
+                            arrayToKeep.Add(parent[i]);
+                        }
+                    }
+
+                    parent.Replace(arrayToKeep);
+
+                    removed = arrayToRemove;
+
+                    break;
+
+                default:
+                    return null;
+            }
+
+            return removed;
         }
     }
 }
