0c6772dff1f476f1ced292097adc06fa0867f80f
ShuzhaoFeng <shuzhao_feng@outlook.com>
2023-09-27 11:54:20 -0400

    Update Globals & Create Silent New Regexes

--- src/SerializerGlobals.cs
+++ src/SerializerGlobals.cs
@@ -4,24 +4,34 @@ namespace JsonPathSerializer
 {
     internal class SerializerGlobals
     {
-        public static readonly Uri JSON_PATH_SERIALIZER_REPOSITORY_URL =
+        internal static readonly Uri JSON_PATH_SERIALIZER_REPOSITORY_URL =
             new("https://github.com/ShuzhaoFeng/JsonPathSerializer");
 
-        public class ErrorMessage
+        internal class ErrorMessage
         {
-            public static readonly string UNSUPPORTED_TOKEN = "The JsonPath contains an unsupported token." +
+            internal static readonly string UNSUPPORTED_TOKEN = "The JsonPath contains an unsupported token." +
                                                         " Please check the documentation at " +
                                                         JSON_PATH_SERIALIZER_REPOSITORY_URL +
                                                         " for supported tokens.";
         }
-        public class JsonPathRegex
+        internal class JsonPathRegex
         {
-            public static readonly Regex PROPERTY_DOT = new Regex(@"\.([^\.\[]+)");
-            public static readonly Regex PROPERTY_BRACKET = new(@"\[\'([^']+)\'\]");
-            public static readonly Regex INDEX = new(@"\[(-?\d+)\]");
-            public static readonly Regex INDEXES = new(@"\[(-?\d+,\s{0,1})+(-?\d+)\]");
-            public static readonly Regex INDEX_SPAN =
+            // old regexes to be removed in v.0.2.0
+            internal static readonly Regex PROPERTY_DOT = new Regex(@"\.([^\.\[]+)");
+            internal static readonly Regex PROPERTY_BRACKET = new(@"\[\'([^']+)\'\]");
+
+            internal static readonly Regex INDEX = new(@"\[(-?\d+)\]");
+            internal static readonly Regex INDEXES = new(@"\[(-?\d+,\s{0,1})+(-?\d+)\]");
+            internal static readonly Regex INDEX_SPAN =
                 new(@"\[(-?\d*)\s{0,1}:\s{0,1}(-?\d*)\]");
+
+            // new regexes to be used in v.0.2.0
+            /*
+            internal static readonly Regex PROPERTY = new(@"\.([^\.\[]+)|\[\'([^']+)\'\]");
+            internal static readonly Regex INDEX = new(@"\[[-,:\s\d]+\]");
+            internal static readonly Regex INDEX_TOKEN = new(@"(-?\d*\s{0,1}:\s{0,1}-?\d*)|(-?\d+)");
+            internal static readonly Regex INDEX_SPAN = new(@"(-?\d*)\s{0,1}:\s{0,1}(-?\d*)");
+            */
         }
     }
 }
