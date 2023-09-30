d32aeb68a3f35d5469e8c043e0f9737a8f6c29d2
ShuzhaoFeng <shuzhao_feng@outlook.com>
2023-09-30 17:35:19 -0400

    Create Silence Index Tokens

--- src/Structs/Path/JsonPathPropertyToken.cs
+++ src/Structs/Path/JsonPathPropertyToken.cs
@@ -2,5 +2,11 @@
 {
     class JsonPathPropertyToken : NewJsonPathToken
     {
+        public string Property { get; }
+
+        public JsonPathPropertyToken(string property)
+        {
+            Property = property;
+        }
     }
 }
