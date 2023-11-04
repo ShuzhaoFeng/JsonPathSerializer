970a48ec266f094b50a0aa62e6f6d0811af48b9a
Shuzhao Feng <90885757+ShuzhaoFeng@users.noreply.github.com>
2023-11-03 20:51:16 -0400

    Add Inline Documentation for JsonNodeToken (#104)

--- src/Structs/JsonNodeToken.cs
+++ src/Structs/JsonNodeToken.cs
@@ -2,8 +2,15 @@
 
 namespace JsonPathSerializer.Structs;
 
+/// <summary>
+///     A JsonNodeToken describes a node of a JsonPath, which corresponds to a level of depth in the Json tree.
+///     It is used to keep track of the last available nodes in the root.
+/// </summary>
 internal class JsonNodeToken
 {
+    /// <summary>
+    ///     The JsonNodeToken constructor.
+    /// </summary>
     public JsonNodeToken(JToken token, int index)
     {
         Token = token;
