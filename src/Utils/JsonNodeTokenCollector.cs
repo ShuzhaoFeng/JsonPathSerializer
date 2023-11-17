f34b252ea67924896645f733fbc1879d96015ba3
Shuzhao Feng <90885757+ShuzhaoFeng@users.noreply.github.com>
2023-11-17 13:40:53 -0500

    Implement CollectToken with Priority (#112)

--- src/Utils/JsonNodeTokenCollector.cs
+++ src/Utils/JsonNodeTokenCollector.cs
@@ -127,6 +127,142 @@ internal class JsonNodeTokenCollector
         Priority priority
     )
     {
-        throw new NotImplementedException();
+        int pathIndex = 0;
+        List<JsonNodeToken> currentTokens = new() { new JsonNodeToken(json, pathIndex) };
+
+        // Navigate through the json tree and keep track of all nodes.
+        // Stop when the path is fully consumed or when all nodes are the last available.
+        while (true)
+        {
+            if (pathIndex >= pathTokens.Count - 1) // Path fully consumed.
+                return currentTokens;
+
+            IJsonPathToken token = pathTokens[pathIndex];
+
+            switch (token)
+            {
+                case JsonPathPropertyToken propertyToken:
+
+                    // at each level of the tree, do the same check for each token.
+                    for (int i = 0; i < currentTokens.Count; i++)
+                    {
+                        JsonNodeToken currentToken = currentTokens[i];
+                        JToken jToken = currentToken.Token;
+
+
+                        if (currentToken.IsLastAvailableToken) // already identified as a last available token.
+                        {
+                            // leave the token as it is.
+                        }
+                        else if (jToken is JObject currentJObject)
+                        {
+                            if (currentJObject.TryGetValue(propertyToken.Property, out JToken? value))
+                            {
+                                // replace the current token with a new one.
+                                currentTokens[currentTokens.IndexOf(currentToken)] =
+                                    new JsonNodeToken
+                                    (
+                                        value ?? throw new NullReferenceException(),
+                                        currentToken.Index + 1
+                                    );
+                            }
+                            else // This is the last available token.
+                            {
+                                currentTokens[i] = currentToken.AsLastAvailable();
+                            }
+                        }
+                        // expecting a JObject where to add property, discovered JArray or JValue instead.
+                        else if (priority == Priority.High)
+                        {
+                            JObject emptyJObject = new();
+                            jToken.Replace(emptyJObject);
+                            currentToken.Token = emptyJObject;
+
+                            currentTokens[i] = currentToken.AsLastAvailable();
+                        }
+                        else
+                        {
+                            throw new ArgumentException
+                            (
+                                $"JSON element $.{jToken.Path} " +
+                                $"is a {jToken.Type}, therefore cannot be taken as an Object."
+                            );
+                        }
+                    }
+
+                    break;
+
+                case JsonPathIndexToken indexToken:
+
+                    // due to index tokens may increase the number of current tokens
+                    // use a new list to store the new tokens to simplify code development.
+                    List<JsonNodeToken> newCurrentTokens = new();
+
+                    // at each level of the tree, do the same check for each token.
+                    for (int i = 0; i < currentTokens.Count; i++)
+                    {
+                        JsonNodeToken currentToken = currentTokens[i];
+                        JToken jToken = currentToken.Token;
+
+
+                        if (currentToken.IsLastAvailableToken) // already identified as a last available token.
+                        {
+                            // leave the token as it is.
+                        }
+                        else if // Check whether the token's children contains the next element of the path.
+                        (
+                            jToken is JArray currentJArray
+                        )
+                        {
+                            if (currentJArray.Count >= indexToken.Bound)
+                            {
+                                for (int j = 0; j < currentJArray.Count; j++) // iterate through each index.
+                                    // Check whether the index specified by the path contains the index.
+                                    if (JsonPathValidator.ArrayContainsIndex(indexToken, j, currentJArray.Count))
+                                        newCurrentTokens.Add(new JsonNodeToken
+                                        (
+                                            currentJArray[j] ?? throw new NullReferenceException(),
+                                            currentToken.Index + 1
+                                        ));
+                            }
+                            else // This is the last available token.
+                            {
+                                newCurrentTokens.Add(currentToken.AsLastAvailable());
+                            }
+                        }
+                        // expecting a JArray where to add index, discovered JObject or JValue instead.
+                        else if (priority == Priority.High)
+                        {
+                            JArray emptyJArray = new();
+                            jToken.Replace(emptyJArray);
+                            currentToken.Token = emptyJArray;
+
+                            currentTokens[i] = currentToken.AsLastAvailable();
+                        }
+                        else
+                        {
+                            throw new ArgumentException
+                            (
+                                $"JSON element $.{jToken.Path} " +
+                                $"is a {jToken.Type}, therefore cannot be taken as an Array."
+                            );
+                        }
+                    }
+
+                    // replace the current tokens with the new ones.
+                    currentTokens = newCurrentTokens;
+
+                    break;
+
+                default:
+                    throw new NotSupportedException(ErrorMessage.UNSUPPORTED_TOKEN);
+            }
+
+            // if all tokens are the last available, no need to search further down.
+            if (currentTokens.Any(x => !x.IsLastAvailableToken))
+                pathIndex++;
+            else
+                return currentTokens;
+        }
     }
 }
