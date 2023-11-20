d927d0c1c23032b5b6d8581cf826078f7e56aa9c
Shuzhao Feng <90885757+ShuzhaoFeng@users.noreply.github.com>
2023-11-19 22:52:35 -0500

    Implement GenerateNewRoot with Priority (#113)

--- src/Utils/JTokenGenerator.cs
+++ src/Utils/JTokenGenerator.cs
@@ -200,6 +200,127 @@ internal class JTokenGenerator
         Priority priority
     )
     {
-        throw new NotImplementedException();
+        // identify the JsonPathToken on the split point.
+        IJsonPathToken splitToken = pathTokens[lastAvailableToken.Index];
+
+        // Generate a new JToken with all its child JsonPathTokens.
+
+        List<IJsonPathToken> unavailableTokens = pathTokens.GetRange
+        (
+            lastAvailableToken.Index + 1,
+            pathTokens.Count - lastAvailableToken.Index - 1
+        );
+
+        JToken newToken = GenerateToken(unavailableTokens, value
+                                                           ?? throw new ArgumentNullException(nameof(value)));
+
+        // merge the new JToken into the root copy using the split JsonPathToken.
+        switch (splitToken)
+        {
+            case JsonPathPropertyToken propertyToken:
+                // check if the corresponding location to add a value contains child elements,
+                // which would be overwritten by the new value.
+
+                if (unavailableTokens.Count == 0)
+                {
+                    // replacing a value that contains child elements
+                    if (priority < Priority.High
+                        && lastAvailableToken.Token[propertyToken.Property] is JContainer jContainer)
+                    {
+                        throw new ArgumentException("JSON element $." +
+                            $"{jContainer.Path} contains a child element," +
+                            "therefore cannot be replaced.");
+                    }
+
+                    // replacing an existing value
+                    if (priority < Priority.Normal
+                        && lastAvailableToken.Token[propertyToken.Property] is not null)
+                    {
+                        throw new ArgumentException("JSON element $." +
+                            $"{lastAvailableToken.Token.Path}.{propertyToken.Property} contains an element," +
+                            "therefore cannot be replaced.");
+                    }
+                }
+
+                JObject lastJObject = (JObject)lastAvailableToken.Token;
+                lastJObject[propertyToken.Property] = newToken;
+
+                break;
+
+            case JsonPathIndexToken indexToken:
+                JArray lastJArray;
+
+                if (lastAvailableToken.Token.HasValues)
+                {
+                    lastJArray = (JArray)lastAvailableToken.Token;
+                }
+                else // empty JObject that has been added as placeholder.
+                {
+                    lastJArray = new JArray();
+
+                    if (lastAvailableToken.Index == 0) // if at root level, directly replace the root.
+                        root = lastJArray;
+                    else if (lastAvailableToken.Token.Parent is not null) lastAvailableToken.Token.Replace(lastJArray);
+                }
+
+                if (unavailableTokens.Count == 0)
+                    for (int i = 0; i < lastJArray.Count; i++) {
+                        bool arrayContainsIndex = JsonPathValidator.ArrayContainsIndex(indexToken, i, lastJArray.Count);
+                        
+                        // check if the corresponding location to add a value contains child elements,
+                        // which would be overwritten by the new value.
+                        if (priority < Priority.High && arrayContainsIndex
+                                                     && lastAvailableToken.Token[i] is JContainer jContainer)
+                        {
+                            throw new ArgumentException("JSON element $." +
+                                $"{jContainer.Path} contains a child element," +
+                                "therefore cannot be replaced.");
+                        }
+
+                        if (priority < Priority.Normal && arrayContainsIndex
+                                                       && lastAvailableToken.Token[i] is not null
+                                                       && lastAvailableToken.Token[i]!.HasValues)
+                        {
+                            throw new ArgumentException("JSON element $." +
+                                $"{lastAvailableToken.Token.Path}[{i}] contains an element," +
+                                "therefore cannot be replaced.");
+                        }
+                    }
+
+                // fill the JArray with empty elements up to the minimum index bound required.
+                for (int i = lastJArray.Count; i < indexToken.Bound; i++) lastJArray.Add(new JObject());
+
+                foreach (IValueContainer container in indexToken.Indexes)
+                    // replace the element at the specified index/index span with the new value.
+
+                    if (container is IndexValueContainer indexValueContainer)
+                    {
+                        int index = indexValueContainer.Index;
+
+                        // if the index is negative, count the index from the end of the array.
+                        lastJArray[index >= 0 ? index : lastJArray.Count + index] = newToken;
+                    }
+                    else if (container is IndexSpanValueContainer indexSpanValueContainer)
+                    {
+                        int start = indexSpanValueContainer.StartIndex;
+
+                        // if the end index is not specified, it is defaulted the last index of the array.
+                        int end = indexSpanValueContainer.EndIndex ??
+                                  (start < 0 ? -1 : lastJArray.Count - 1);
+
+                        int min = Math.Min(start, end);
+                        int max = Math.Max(start, end);
+
+                        // replace the value across the span.
+                        for (int i = min; i <= max; i++) lastJArray[i >= 0 ? i : lastJArray.Count + i] = newToken;
+                    }
+
+                break;
+
+            default:
+                throw new NotSupportedException(ErrorMessage.UNSUPPORTED_TOKEN);
+        }
+
+        return root;
     }
 }
